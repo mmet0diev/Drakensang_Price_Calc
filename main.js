882665649347158e9da2a8e14cae77e6ed7ab87c
mmet0diev <max.metodiev.1999@gmail.com>
2025-05-15 12:32:27 +0300

    testing displaying js in index.html

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import {premDaysToCash, andersToCash, shinyDustToCash} from "./functions.js"
+import {premDaysToCash, andersToCash, shinyDustToCash, getIPsToAnders} from "./functions.js"
+
+// function displayPremDaysToCashRes(page_element){
+//     document.getElementById(page_element).textContent = premDaysToCash(200, "BG_EU")
+// }
+
+window.displayIpsInCash = function(page_element){
+    document.getElementById(page_element).textContent = parseInt(getIPsToAnders(2000))
+}
