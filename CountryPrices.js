9b4c992ff9767c27dc92e286c8ae4334c5d36727
mmet0diev <max.metodiev.1999@gmail.com>
2025-05-30 20:30:18 +0300

    .

--- CountryPrices.js
+++ CountryPrices.js
@@ -1,14 +1,6 @@
 export const countryPrices = {
   // can be changed accodingly to the "main" game currency
-  "BG_EU": {
-    "prem_per_month": 7.59,
-    "deluxe_per_month": 16.49,
-    "midDustPack": 7.59,
-    "bigDustPack": 37.99,
-    "averageAndermant": Math.ceil(28742.85714285714),
-    "averageAnderCost": Math.ceil(20.86714285714286),
-    // "drakenCostInAverageAnders" : 8000 / Math.ceil(28742.85714285714) * Math.ceil(20.86714285714286)
-  },
+  
   "TL_EU": {
     "prem_per_month": 4.50,
     "deluxe_per_month": 10.12,
@@ -16,6 +8,15 @@ export const countryPrices = {
     "bigDustPack": 22.95,
     "averageAndermant": Math.ceil(33333.33333333333),
     "averageAnderCost": Math.ceil(14.70),
+    // "drakenCostInAverageAnders" : 8000 / Math.ceil(28742.85714285714) * Math.ceil(20.86714285714286)
+  },
+  "BG_EU": {
+    "prem_per_month": 7.59,
+    "deluxe_per_month": 16.49,
+    "midDustPack": 7.59,
+    "bigDustPack": 37.99,
+    "averageAndermant": Math.ceil(28742.85714285714),
+    "averageAnderCost": Math.ceil(20.86714285714286),
     // "drakenCostInAverageAnders" : 8000 / Math.ceil(28742.85714285714) * Math.ceil(20.86714285714286)
   },
   // "UK_GBP": {
