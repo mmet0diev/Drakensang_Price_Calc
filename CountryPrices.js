a9f14d68fbc8f95a20e957a3283b9f72d5ce595a
mmet0diev <max.metodiev.1999@gmail.com>
2025-05-30 19:50:29 +0300

    tl prices added

--- CountryPrices.js
+++ CountryPrices.js
@@ -9,6 +9,15 @@ export const countryPrices = {
     "averageAnderCost": Math.ceil(20.86714285714286),
     // "drakenCostInAverageAnders" : 8000 / Math.ceil(28742.85714285714) * Math.ceil(20.86714285714286)
   },
+  "TR_EU": {
+    "prem_per_month": 4.50,
+    "deluxe_per_month": 10.12,
+    "midDustPack": 4.50,
+    "bigDustPack": 22.95,
+    "averageAndermant": Math.ceil(33333.33333333333),
+    "averageAnderCost": Math.ceil(14.70),
+    // "drakenCostInAverageAnders" : 8000 / Math.ceil(28742.85714285714) * Math.ceil(20.86714285714286)
+  },
   // "UK_GBP": {
   //   "prem_per_month": ("BG_EU".prem_per_month * rate).toFixed(2),
   //   "deluxe_per_month": ("BG_EU".deluxe_per_month * rate).toFixed(2),
