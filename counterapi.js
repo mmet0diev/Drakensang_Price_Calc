3f442f089331e6fe5fe5b982e65331379d2a80f6
mmet0diev <max.metodiev.1999@gmail.com>
2025-06-03 10:26:55 +0300

    trying new counterapi

---
+++ counterapi.js
@@ -0,0 +1,12 @@
+const calcAppName = "mmet0diev.github.io/Drakensang_Price_Calc";
+const apiurl = `https://api.countapi.xyz/create?namespace=${calcAppName}&key=visits&value=0`;
+
+// Create a counter (first time only) in your browser console:
+fetch(apiurl)
+
+// Increment and get the count on each visit:
+fetch(apiurl)
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('visitCounter').textContent = data.value;
+  });
