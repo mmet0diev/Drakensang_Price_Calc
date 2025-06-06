2b18b4313baf6462637b7c1ff6acbde712cb145f
mmet0diev <max.metodiev.1999@gmail.com>
2025-06-06 10:48:12 +0300

    counter api integrated

--- counterapi.js
+++ counterapi.js
@@ -1,14 +1,14 @@
-export function displayVisitsCount() {
-  const namespace = "mmet0diev_namespace";
-  const key = "calc_app_count";
+// export function displayVisitsCount() {
+//   const namespace = "mmet0diev_namespace";
+//   const key = "calc_app_count";
 
-  const url = `https://api.countapi.store/hit/${namespace}/${key}`;
+//   const url = `https://api.countapi.store/hit/${namespace}/${key}`;
 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      document.getElementById("visitCounter").textContent = data.value;
-    })
-    .catch(err => console.error("Visit counter error:", err));
-}
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data);
+//       document.getElementById("visitCounter").textContent = data.value;
+//     })
+//     .catch(err => console.error("Visit counter error:", err));
+// }
